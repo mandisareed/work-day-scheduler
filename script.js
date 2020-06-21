f50d697e524edce23c1bf8a5e4e50b57167a0225
Mandisa Reed <mandisa.reed@gmail.com>
2020-06-20 16:54:12 -1000

    saved input to local storage, returned and displayed value when page is refreshed

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function () { 
 
   var textArea = $("#textarea-a");
   var textAreaOutput = $("text-output");
 
   var todos = [];
 
-  var submittedTask = {
-    task:"" //userInput
-  };
-
-  var storedTasks = [];
-  storedTasks.push(submittedTask);
-
-  var submittedTask = JSON.stringify(storedTasks);
-  localStorage.setItem("StoredTasks", storedTasks);
 
   //Implement a function to display the current day, date, and time that updates every second
   var now = $("#currentDay");
@@ -21,20 +12,33 @@ $(document).ready(function () {
     now.text(moment().format("LL h:mm:ss"));
   }, 1000);//end setInterval
 
-  //invoke function to read what's in storage when the page loads
-  readLocalStorage();
-
   // create a function to read and retrieve tasks from local storage when the page first loads
   function readLocalStorage() {
     var storedTasksString = localStorage.getItem("StoredTasks");
-    var storedTasks = [];
+    var storedTasks = [
+        {"task": ""}, {"task": ""}, {"task": ""}, {"task": ""}, {"task": ""},
+        {"task": ""}, {"task": ""}, {"task": ""}, {"task": ""}
+    ];
     //if there is something in local storage, parse the string of text into an object
     if (storedTasksString !== null) {
       storedTasks = JSON.parse(storedTasksString);
     }
+    return storedTasks;
   }//end readLocalStorage() fct def
 
+function displayResults(){
+ //invoke function to read what's in storage when the page loads
+ var storedTasks = readLocalStorage();
+    ///create a for loop to loop through the array of stored user inputs for each time block
+  for (var i = 0; i < storedTasks.length; i++) {
+    var storedTask = storedTasks[i];
+    $("textarea")[i].value = storedTask.task;
+  }
+}//end displayResults() fct def
 
+  //invoke function to display what's in Local Storage
+  displayResults();
+  
   //When the save button is clicked, create an element in which the text in the input area will be contained
   $(".saveBtn").on("click", function (event) {
     event.preventDefault();
@@ -44,19 +48,14 @@ $(document).ready(function () {
     var userInput = $("#textarea-a").val();
     console.log("New Submission: \n", userInput);
 
-    //append userInput to #text-output
-    $("#text-output").append(userInput);
-
+    //save input to local storage
+    var storedTasks = readLocalStorage();
+    var newTask = {"task": userInput};
+    storedTasks[0] = newTask;
+    
+    localStorage.setItem("StoredTasks", JSON.stringify(storedTasks));
+    displayResults();
   });
-
-
-function displayResults(){
-  for (var i = 0; i < storedTasks.length; i++) {
-    var storedTask = storedTasks[i];
-    var listEl = $("<li>");
-    listEl.text(storedTask.task);
-    todoInput.append(listEl);
-  }
-}//end displayResults() fct def
  
 }); //end ready fct declaration
+
